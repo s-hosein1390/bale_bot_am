b1afc62ca550ac4efcf7179bfd4177570ea23f0f
s-hosein1390 <karbasi161@gmail.com>
2025-10-20 23:02:46 +0330

    fix: run on 0.0.0.0 for render
    Update app.py to support dynamic port configuration.

--- app.py
+++ app.py
@@ -66,4 +66,6 @@ def webhook():
     return "OK", 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
